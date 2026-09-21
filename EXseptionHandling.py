f40f0a5444370c3c3977fe2948cdaf44a06abbab
rahulgayke1704 <rahulgayke1704@gmail.com>
2026-09-21 16:25:01 +0530

    new

---
+++ EXseptionHandling.py
@@ -0,0 +1,26 @@
+"""try:
+    premium = int(input("Enter Premium amount:"))
+    print("Premium:",premium)
+
+except:
+    print("Invalid Premium amount ") """
+#************************************************************************************
+try:
+    age = float(input("Enter your age: "))
+    vage = 18 - age
+
+    if age <= 0:
+        raise ValueError("Age must be greater than zero.")
+
+    elif age >= 18:
+        print("Allow to vote")
+
+    else:
+        print("Not allowed to vote")
+        raise ValueError(f"Wait for {vage} years")     #give manu
+
+except ValueError as e:
+    print("Error:", e)
+
+#***************************************************************
+
